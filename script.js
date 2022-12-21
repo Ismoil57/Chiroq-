652f6ee38f956c3e48fd1efb093b3e557e7f6287
Ismoil57 <mismoil7757@GMAIL.COM>
2022-12-21 20:29:10 +0500

    Chiroq

---
+++ script.js
@@ -0,0 +1,202 @@
+// // ----- ROOM -----
+// let room = document.querySelector(".room");
+
+// setInterval(() => {
+//   let src = room.getAttribute("src");
+//   if (src === "./image/room-on.jpg") {
+//       room.setAttribute("src", "./image/room-off.jpg");
+//     } else {
+//     room.setAttribute("src", "./image/room-on.jpg");
+// }
+// }, 1500);
+
+// // ------- LAMP1 ---------
+// let lamp1 = document.querySelector(".lamp1");
+
+// setInterval(() => {
+//   let src = lamp1.getAttribute("src");
+//   if (src === "./image/chiroq-on.png") {
+//     lamp1.setAttribute("src", "./image/chiroq-off.png");
+//   } else {
+//     lamp1.setAttribute("src", "./image/chiroq-on.png");
+//   }
+// }, 1500);
+// // ------- LAMP2 ---------
+// let lamp2 = document.querySelector(".lamp2");
+
+// setInterval(() => {
+//   let src = lamp2.getAttribute("src");
+//   if (src === "./image/chiroq-on.png") {
+//     lamp2.setAttribute("src", "./image/chiroq-off.png");
+//   } else {
+//     lamp2.setAttribute("src", "./image/chiroq-on.png");
+//   }
+// }, 1500);
+
+// // ------- LAMP3 ---------
+// let lamp3 = document.querySelector(".lamp3");
+
+// setInterval(() => {
+//   let src = lamp3.getAttribute("src");
+//   if (src === "./image/chiroq-on.png") {
+//     lamp3.setAttribute("src", "./image/chiroq-off.png");
+//   } else {
+//     lamp3.setAttribute("src", "./image/chiroq-on.png");
+//   }
+// }, 1500);
+
+// // ------- LAMP4 ---------
+// let lamp4 = document.querySelector(".lamp4");
+
+// setInterval(() => {
+//   let src = lamp4.getAttribute("src");
+//   if (src === "./image/chiroq-on.png") {
+//     lamp4.setAttribute("src", "./image/chiroq-off.png");
+//   } else {
+//     lamp4.setAttribute("src", "./image/chiroq-on.png");
+//   }
+// }, 1500);
+
+// // ------- LAMP5 ---------
+// let lamp5 = document.querySelector(".lamp5");
+
+// setInterval(() => {
+//   let src = lamp5.getAttribute("src");
+//   if (src === "./image/chiroq-on.png") {
+//     lamp5.setAttribute("src", "./image/chiroq-off.png");
+//   } else {
+//     lamp5.setAttribute("src", "./image/chiroq-on.png");
+//   }
+// }, 1500);
+
+// ==============================================================
+
+//  ==== ON ====
+
+function btnOn() {
+    // ----- ROOM -----
+    let room = document.querySelector(".room");
+  
+    {
+      let src = room.getAttribute("src");
+      if (src === "./image/room-off.jpg") {
+        room.setAttribute("src", "./image/room-on.jpg");
+      }
+    }
+  
+    // ------- LAMP1 ---------
+    let lamp1 = document.querySelector(".lamp1");
+  
+    {
+      let src = lamp1.getAttribute("src");
+      if (src === "./image/chiroq-off.png") {
+        lamp1.setAttribute("src", "./image/chiroq-on.png");
+      }
+    }
+    // ------- LAMP2 ---------
+    let lamp2 = document.querySelector(".lamp2");
+  
+    {
+      let src = lamp2.getAttribute("src");
+      if (src === "./image/chiroq-off.png") {
+        lamp2.setAttribute("src", "./image/chiroq-on.png");
+      }
+    }
+  
+    // ------- LAMP3 ---------
+    let lamp3 = document.querySelector(".lamp3");
+  
+    {
+      let src = lamp3.getAttribute("src");
+      if (src === "./image/chiroq-off.png") {
+        lamp3.setAttribute("src", "./image/chiroq-on.png");
+      }
+    }
+  
+    // ------- LAMP4 ---------
+    let lamp4 = document.querySelector(".lamp4");
+  
+    {
+      let src = lamp4.getAttribute("src");
+      if (src === "./image/chiroq-off.png") {
+        lamp4.setAttribute("src", "./image/chiroq-on.png");
+      }
+    }
+  
+    // ------- LAMP5 ---------
+    let lamp5 = document.querySelector(".lamp5");
+  
+    {
+      let src = lamp5.getAttribute("src");
+      if (src === "./image/chiroq-off.png") {
+        lamp5.setAttribute("src", "./image/chiroq-on.png");
+      }
+    }
+  }
+  
+  
+  
+  // ====== OFF ======
+  
+  
+  
+  function btnOff() {
+        // ----- ROOM -----
+    let room = document.querySelector(".room");
+  
+    {
+      let src = room.getAttribute("src");
+      if (src === "./image/room-on.jpg") {
+        room.setAttribute("src", "./image/room-off.jpg");
+      }
+    }
+  
+    // ------- LAMP1 ---------
+    let lamp1 = document.querySelector(".lamp1");
+  
+    {
+      let src = lamp1.getAttribute("src");
+      if (src === "./image/chiroq-on.png") {
+        lamp1.setAttribute("src", "./image/chiroq-off.png");
+      }
+    }
+    // ------- LAMP2 ---------
+    let lamp2 = document.querySelector(".lamp2");
+  
+    {
+      let src = lamp2.getAttribute("src");
+      if (src === "./image/chiroq-on.png") {
+        lamp2.setAttribute("src", "./image/chiroq-off.png");
+      }
+    }
+  
+    // ------- LAMP3 ---------
+    let lamp3 = document.querySelector(".lamp3");
+  
+    {
+      let src = lamp3.getAttribute("src");
+      if (src === "./image/chiroq-on.png") {
+        lamp3.setAttribute("src", "./image/chiroq-off.png");
+      }
+    }
+  
+    // ------- LAMP4 ---------
+    let lamp4 = document.querySelector(".lamp4");
+  
+    {
+      let src = lamp4.getAttribute("src");
+      if (src === "./image/chiroq-on.png") {
+        lamp4.setAttribute("src", "./image/chiroq-off.png");
+      }
+    }
+  
+    // ------- LAMP5 ---------
+    let lamp5 = document.querySelector(".lamp5");
+  
+    {
+      let src = lamp5.getAttribute("src");
+      if (src === "./image/chiroq-on.png") {
+        lamp5.setAttribute("src", "./image/chiroq-off.png");
+      }
+    }
+  }
